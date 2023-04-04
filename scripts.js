e21c6d23307346b93d606ccdb6e4248d43fe9e27
fatinha33 <fatimaazevedo591@gmail.com>
2023-04-04 11:16:01 -0400

    primeiro commit-projeto responsividade

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+
+function trocacor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+
+trocacor
+}
+function trocaimagem(imagem){
+    let iphone = document.querySelector(".iphone")
+    iphone.src = imagem
+}
+
+            
